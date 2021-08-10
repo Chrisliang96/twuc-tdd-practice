d9ef54d8cd35b1ee91b26f472cdb31c5b57c9ca9
梁寒冰 <lianghanbing@lianghanbings-MacBook-Pro.local>
2021-08-10 11:07:10 +0800

    [hanbing liang]test result if input is null or less than 30

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MommifierTest {
+    //aeiou
+    public String transform(String input){
+        if (input.isEmpty()){
+            return null;
+        }
+        return input ;
+
+    }
+    @Test
+    public void should_return_input_if_vowel_less_than_30present(){
+        String input = "b";
+        Assertions.assertEquals(input,transform(input));
+    }
+
+    @Test
+    public void should_return_input_if_vowel_is_null(){
+        String input =" ";
+        Assertions.assertEquals(input,transform(input));
+    }
+
+
+
+
 }
