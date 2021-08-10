d65ac704aef8a81eccb0a6eda9ff7c5925510ffe
梁寒冰 <lianghanbing@lianghanbings-MacBook-Pro.local>
2021-08-10 11:14:33 +0800

    [feat]:transform vowel

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -7,11 +7,14 @@ public class MommifierTest {
         if (input.isEmpty()){
             return null;
         }
+        if (input.matches("a")){
+            return "mommy";
+        }
         return input ;
 
     }
     @Test
-    public void should_return_input_if_vowel_less_than_30present(){
+    public void should_return_input_if_exist_one(){
         String input = "b";
         Assertions.assertEquals(input,transform(input));
     }
@@ -22,6 +25,11 @@ public class MommifierTest {
         Assertions.assertEquals(input,transform(input));
     }
 
+    @Test
+    public void should_return_transform_if_exist_one_vowel(){
+        String input = "a";
+        Assertions.assertEquals("mommy",transform(input));
+    }
 
 
 
